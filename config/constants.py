e50ac69af2aa256957789a2588e187f57d9771db
Joseph Reed <atljoereed@gmail.com>
2016-10-23 16:54:11 -0400

    Adding in settings.py into .gitignore file.

--- config/constants.py
+++ config/constants.py
@@ -1,5 +1,5 @@
 base_url = 'https://www.irs.gov/pub/irs-soi/'
 
-path = '/Users/jreed/Development/data_loader/data/'
+path = '/Users/jreed/Development/database_utils/data/'
 
 files = ['eo1.csv', 'eo2.csv', 'eo3.csv', 'eo4.csv']
